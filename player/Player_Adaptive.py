038fa354b9c083cd2f334673be50151ba958f25e
Heather Blakeway <heatheranneblakeway@gmail.com>
2024-09-02 23:16:43 +1000

    Create adaptive class for rules

--- player/Player_Adaptive.py
+++ player/Player_Adaptive.py
@@ -1,1 +1,17 @@
-import dynrules
+import sys
+from dynrules import RuleSet, Rule, LearnSystem
+
+class CarcassonneAdaptive:
+    """
+    1. Average Time Strategy 
+    2. Opponent Winning Strategy
+    3. Player Winning Strategy 
+    4. Fields Strategy 
+    """
+
+def run():
+    adaptive = CarcassonneAdaptive()
+
+
+if __name__ == "__main__":
+    sys.exit(run())
